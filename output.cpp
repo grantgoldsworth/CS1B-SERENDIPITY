44e39f3699aa835f6e26154f5a9f13d855527fce
Grant Goldsworth <goldsworthgrant@gmail.com>
2020-04-26 10:52:00 -0700

    A19 semi-final 3 - working on retail

---
+++ output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main () {
+	
+	    cout << left;
+        cout << setw(50) << "TITLE" << setw(25) << "ISBN" << setw(25) << "QTY O/H" << setw(15) << "WHOLESALE COST" << endl;
+        cout << "-----------------------------------------------   ----------               -------                  --------------\n";
+				
+				return 0;
+}
